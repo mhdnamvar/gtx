20d5c34ac8c2ff10d8de81908f87d9559f674fe4
Mohammad Namvar <namvar@gmail.com>
2019-07-11 22:30:57 +0200

    Adding LLANumeric type

---
+++ LLANumeric_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_LLANumeric_1(t *testing.T) {
+	value := "12345"
+	expected := []byte{0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35}
+	codec := LLANumeric{Field{"", "Should be 3030303030303132333435", 11}, true}
+	actual, _ := codec.Encode(value)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("actual: %x, expected: %x\n", actual, expected)
+	}
+}
+
+func Test_LLANumeric_2(t *testing.T) {
+	value := "12345"
+	expected := []byte{0x31, 0x32, 0x33, 0x34, 0x35}
+	codec := LLANumeric{Field{"", "Should be 3132333435", 11}, false}
+	actual, _ := codec.Encode(value)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("actual: %x, expected: %x\n", actual, expected)
+	}
+}
+
+func Test_LLANumeric_3(t *testing.T) {
+	value := "123456789012"
+	codec := LLANumeric{Field{"", "Should return error", 11}, false}
+	expected, err := codec.Encode(value)
+
+	if err == nil {
+		t.Errorf("Should return error\n")
+	}
+
+	if !reflect.DeepEqual(err.Error(), Errors[InvalidLengthError].message) {
+		t.Errorf("Should return error\n")
+	}
+	if expected != nil {
+		t.Errorf("Should return nil\n")
+	}
+}
+
+func Test_LLANumeric_4(t *testing.T) {
+	value := "1234567890A"
+	codec := LLANumeric{Field{"", "Should return nil, error", 11}, true}
+	expected, err := codec.Encode(value)
+	if !reflect.DeepEqual(err.Error(), Errors[NumberFormatError].message) {
+		t.Errorf("Should return error\n")
+	}
+	if expected != nil {
+		t.Errorf("Should return nil\n")
+	}
+}
