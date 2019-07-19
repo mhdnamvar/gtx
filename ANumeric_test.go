f968c8c2419d888d8bc23cc0225abecd71d8a0f0
Mohammad Namvar <mnamvar@rambus.com>
2019-07-19 15:52:23 +0200

    Updating unit tests

--- ANumeric_test.go
+++ ANumeric_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
@@ -10,12 +9,7 @@ func Test_ANumeric_encode(t *testing.T) {
 	expected := []byte("0012345") //[]byte{0x30, 0x30, 0x31, 0x032, 0x33, 0x34, 0x35}
 	codec := ANumeric{"", "Should be 30303132333435", 7}
 	actual, err := codec.Encode(value)
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("actual: %x, expected: %x\n", actual, expected)
-	}
+	checkEncodeResult(t, expected, actual, err)
 }
 
 func Test_ANumeric_decode(t *testing.T) {
@@ -23,10 +17,5 @@ func Test_ANumeric_decode(t *testing.T) {
 	expected := "12345"
 	codec := ANumeric{"", "Should be 12345", 7}
 	actual, err := codec.Decode(value)
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("actual: %x, expected: %x\n", actual, expected)
-	}
+	checkDecodeResult(t, expected, actual, err)
 }
