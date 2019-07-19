335bf81bbd4a5463fd6215ab208e6454573fdb54
Mohammad Namvar <mnamvar@rambus.com>
2019-07-19 11:21:12 +0200

    Updating LLBNumeric decoder

--- LLBNumeric.go
+++ LLBNumeric.go
@@ -18,28 +18,30 @@ func (codec *LLBNumeric) Encode(s string) ([]byte, error) {
 	if len(s) > codec.Length || len(s) > 99 {
 		return nil, Errors[InvalidLengthError]
 	}
-
 	n := new(big.Int)
 	n, ok := n.SetString(s, 10)
 	if !ok {
 		return nil, Errors[NumberFormatError]
 	}
-
 	if codec.Padding {
 		s = LeftPad2Len(s, "0", codec.Length)
 	}
-	// length := IntToBcd(uint64(len(s)))
-	// b := []byte(s)
-	// b = append(length, b...)
-
-	return StrToBcd(s), nil
+	return append(StrToBcd(LeftPad2Len(strconv.Itoa(len(s)), "0", 2)), StrToBcd(s)...), nil
 }
 
 // Decode ...
 func (codec *LLBNumeric) Decode(b []byte) (string, error) {
-	if len(b) > codec.Length || len(b) > 99 {
+	if len(b) < 3 {
 		return "", Errors[InvalidLengthError]
 	}
-	s := strconv.FormatUint(BcdToInt(b), 10)
-	return LeftPad2Len(s, "0", codec.Length), nil
+	length := BcdToInt(b[:1])
+	if length%2 != 0 {
+		length = length/2+1
+	} else {
+		length = length/2
+	}
+	if length <= 0 || uint64(len(b)) < length {
+		return "", Errors[InvalidLengthError]
+	}
+	return strconv.FormatUint(BcdToInt(b[1:length+1]), 10), nil
 }
--- LLBNumeric_test.go
+++ LLBNumeric_test.go
@@ -1,77 +1,43 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
-func Test_LLBNumeric_1(t *testing.T) {
+func Test_LLBNumeric_EncodePad(t *testing.T) {
 	value := "12345"
-	expected := []byte{0x00, 0x00, 0x00, 0x01, 0x23, 0x45}
-	codec := LLBNumeric{"", "Should be 00000012345", 11, true}
+	expected := []byte{0x11, 0x00, 0x00, 0x00, 0x01, 0x23, 0x45}
+	codec := LLBNumeric{"", "Should be 1100000012345", 11, true}
 	actual, err := codec.Encode(value)
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("actual: %x, expected: %x\n", actual, expected)
-	}
+	checkEncodeResult(t, expected, actual, err)
 }
 
-func Test_LLBNumeric_2(t *testing.T) {
+func Test_LLBNumeric_EncodeNoPad(t *testing.T) {
 	value := "12345"
-	expected := []byte{0x01, 0x23, 0x45}
-	codec := LLBNumeric{"", "Should be 012345", 11, false}
+	expected := []byte{0x05, 0x01, 0x23, 0x45}
+	codec := LLBNumeric{"", "Should be 05012345", 11, false}
 	actual, err := codec.Encode(value)
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("actual: %x, expected: %x\n", actual, expected)
-	}
+	checkEncodeResult(t, expected, actual, err)
 }
 
-func Test_LLBNumeric_3(t *testing.T) {
+func Test_LLBNumeric_EncodeInvalidLen(t *testing.T) {
 	value := "12345"
 	codec := LLBNumeric{"", "Should return error", 4, false}
 	actual, err := codec.Encode(value)
-	if err == nil {
-		t.Errorf("Should return error\n")
-	}
-	if !reflect.DeepEqual(err.Error(), Errors[InvalidLengthError].message) {
-		t.Errorf("Should return error\n")
-	}
-	if actual != nil {
-		t.Errorf("Should return nil\n")
-	}
+	checkEncodeError(t, actual, err, InvalidLengthError)
 }
 
-func Test_LLBNumeric_4(t *testing.T) {
+func Test_LLBNumeric_EncodeWrongFormat(t *testing.T) {
 	value := "12345ABC"
 	codec := LLBNumeric{"", "Should return nil, error", 9, true}
 	actual, err := codec.Encode(value)
-	if err == nil {
-		t.Errorf(err.Error())
-	}
-	if !reflect.DeepEqual(err.Error(), Errors[NumberFormatError].message) {
-		t.Errorf("Should return error\n")
-	}
-	if actual != nil {
-		t.Errorf("Should return nil\n")
-	}
+	checkEncodeError(t, actual, err, NumberFormatError)
 }
 
-func Test_LLBNumeric_5(t *testing.T) {
-	value := []byte{0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00,
-		0x01, 0x23, 0x45}
-	expected := "00000012345"
-	codec := LLBNumeric{"", "Should be 00000012345", 11, true}
+func Test_LLBNumeric_Decode(t *testing.T) {
+	value := []byte{0x11, 0x00, 0x00, 0x00, 0x01, 0x23, 0x45}
+	expected := "12345"
+	codec := LLBNumeric{"", "Should be 12345", 11, true}
 	actual, err := codec.Decode(value)
-	if err != nil {
-		t.Errorf("Should return value\n")
-	}
-	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("actual: %x, expected: %x\n", actual, expected)
-	}
+	checkDecodeResult(t, expected, actual, err)
 }
