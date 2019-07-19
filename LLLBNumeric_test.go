34a4bde74f0ad79d91c33a39f04fc5e233053d2f
Mohammad Namvar <mnamvar@rambus.com>
2019-07-19 12:27:21 +0200

    Updating unit tests

--- LLLBNumeric_test.go
+++ LLLBNumeric_test.go
@@ -5,30 +5,24 @@ import (
 	"testing"
 )
 
-func Test_LLLBNumeric_1(t *testing.T) {
+func Test_LLLBNumeric_Encode(t *testing.T) {
 	value := "12345"
-	expected := []byte{
+	expected := []byte{0x01, 0x03,
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
 		0x23, 0x45}
-	codec := LLLBNumeric{"",
-		"Should be 0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000012345",
+	codec := LLLBNumeric{"", "Should be 1030000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000012345",
 		103, true}
 	actual, err := codec.Encode(value)
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("actual: %x, expected: %x\n", actual, expected)
-	}
+	checkEncodeResult(t, expected, actual, err)
 }
 
-func Test_LLLBNumeric_2(t *testing.T) {
+func Test_LLLBNumeric_EncodeNoPad(t *testing.T) {
 	value := "12345"
-	expected := []byte{0x01, 0x23, 0x45}
+	expected := []byte{0x05, 0x01, 0x23, 0x45}
 	codec := LLLBNumeric{"", "Should be 012345", 103, false}
 	actual, err := codec.Encode(value)
 	if err != nil {
@@ -39,34 +33,21 @@ func Test_LLLBNumeric_2(t *testing.T) {
 	}
 }
 
-func Test_LLLBNumeric_3(t *testing.T) {
+func Test_LLLBNumeric_InvalidLen(t *testing.T) {
 	value := "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901"
 	codec := LLLBNumeric{"", "Should return LLL length error", 100, false}
 	actual, err := codec.Encode(value)
-	if err == nil {
-		t.Errorf("Should return error\n")
-	}
-	if !reflect.DeepEqual(err.Error(), Errors[InvalidLengthError].message) {
-		t.Errorf("Should return invalid length error\n")
-	}
-	if actual != nil {
-		t.Errorf("Should return nil\n")
-	}
+	checkEncodeError(t, actual, err, InvalidLengthError)
 }
 
-func Test_LLLBNumeric_4(t *testing.T) {
+func Test_LLLBNumeric_InvalidNumber(t *testing.T) {
 	value := "12345ABC"
 	codec := LLLBNumeric{"", "Should return nil, error", 199, true}
 	actual, err := codec.Encode(value)
-	if !reflect.DeepEqual(err.Error(), Errors[NumberFormatError].message) {
-		t.Errorf("Should return error\n")
-	}
-	if actual != nil {
-		t.Errorf("Should return nil\n")
-	}
+	checkEncodeError(t, actual, err, NumberFormatError)
 }
 
-func Test_LLLBNumeric_5(t *testing.T) {
+func Test_LLLBNumeric_InvalidLen2(t *testing.T) {
 	value := `1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890` +
 		`1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890` +
 		`1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890` +
@@ -79,32 +60,26 @@ func Test_LLLBNumeric_5(t *testing.T) {
 		`1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890`
 	codec := LLLBNumeric{"", "Should return error", 999, false}
 	actual, err := codec.Encode(value)
-	if err == nil {
-		t.Errorf("Should return error\n")
-	}
-	if !reflect.DeepEqual(err.Error(), Errors[InvalidLengthError].message) {
-		t.Errorf("Should return error\n")
-	}
-	if actual != nil {
-		t.Errorf("Should return nil\n")
-	}
+	checkEncodeError(t, actual, err, InvalidLengthError)
 }
 
-func Test_LLLBNumeric_6(t *testing.T) {
-	value := []byte{
+func Test_LLLBNumeric_DecodePdded(t *testing.T) {
+	value := []byte{0x01, 0x03,
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
 		0x23, 0x45}
-	expected := "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000012345"
-	codec := LLLBNumeric{"", "Should be 0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000012345", 103, true}
+	expected := "12345"
+	codec := LLLBNumeric{"", "Should be 12345", 103, true}
 	actual, err := codec.Decode(value)
-	if err != nil {
-		t.Errorf("Should return value\n")
-	}
-	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("actual: %x, expected: %x\n", actual, expected)
-	}
+	checkDecodeResult(t, expected, actual, err)
+}
+func Test_LLLBNumeric_DecodeNoPad(t *testing.T) {
+	value := []byte{0x00, 0x05, 0x01, 0x23, 0x45}
+	expected := "12345"
+	codec := LLLBNumeric{"", "Should be 12345", 103, true}
+	actual, err := codec.Decode(value)
+	checkDecodeResult(t, expected, actual, err)
 }
