18c534ba09b3abc6b13f4994506b3b1846ee9a0e
Mohammad Namvar <mnamvar@rambus.com>
2019-07-25 13:43:49 +0200

    Adding BBinary codec

--- BBinary.go
+++ BBinary.go
@@ -4,29 +4,29 @@ import (
 	"encoding/hex"
 	"fmt"
 )
-// ABinary ...
-type ABinary struct {
+// BBinary ...
+type BBinary struct {
 	Name        string
 	Description string
 	Length      int
 }
 
 // Encode ...
-func (codec *ABinary) Encode(s string) ([]byte, error) {
+func (codec *BBinary) Encode(s string) ([]byte, error) {
 	if len(s)%2 != 0 || len(s)/2 != codec.Length {
 		return nil, Errors[InvalidLengthError]
 	}
-	data, err := hex.DecodeString(s)
+	b, err := hex.DecodeString(s)
 	if err != nil {
 		return nil, Errors[InvalidDataError]
 	}
-	return  data, nil
+	return  b, nil
 }
 
 // Decode ...
-func (codec *ABinary) Decode(b []byte) (string, error) {
+func (codec *BBinary) Decode(b []byte) (string, error) {
 	if len(b) < codec.Length {
 		return "", Errors[InvalidLengthError]
 	}
-	return fmt.Sprintf("%X", b), nil
+	return fmt.Sprintf("%X", b[:codec.Length]), nil
 }
