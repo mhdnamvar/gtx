11192c33b5867b98fe6ce036164ceba22e2f2c61
Mohammad Namvar <namvar@gmail.com>
2019-07-31 20:53:09 +0200

    Fixing ascii protocol unit test

--- Ascii87Protocol_test.go
+++ Ascii87Protocol_test.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func Test_Ascii87_AChar(t *testing.T) {
-	for _, codec := range ASCII1987 {
-		fmt.Printf("%-8s%-20T%-40s%-5d%-3t\n", 
-			codec.GetName(),
-			codec,
-			codec.GetDescription(),
-			codec.GetLength(),
-			codec.GetPadding())
-	}
-	fmt.Println()
 	value := "AB"
-	expected := []byte("AB  ")
-	mtiCodec := ASCII1987[0]
-	actual, err := mtiCodec.Encode(value)
+	expected := []byte("AB          ")
+	codec := ASCII1987[37] //RRN
+	actual, err := codec.Encode(value)
 	assertEqual(t, nil, err)
 	assertEqual(t, expected, actual)
 }
