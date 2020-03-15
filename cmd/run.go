48ad90f9d6ff20c85f9db2f065e32b313029076b
Mohammad Namvar <namvar@gmail.com>
2020-03-15 15:11:06 +0100

    Adding echo server

--- cmd/run.go
+++ cmd/run.go
@@ -1,43 +1,81 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
 
 var (
 	port   int
+	echo   bool
 	runCmd = &cobra.Command{
 		Use:   "run",
 		Short: "runs a gtx server",
 		Long:  ``,
-		Run: func(cmd *cobra.Command, args []string) {
-			run()
-		},
+		Run:   checkRunFlags,
 	}
 )
 
 func init() {
 	rootCmd.AddCommand(runCmd)
 	runCmd.Flags().IntVarP(&port, "port", "p", 8583, "Listening port")
+	runCmd.Flags().BoolVarP(&echo, "echo-server", "e", false, "Run a echo server")
 }
 
-func run() {
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
-	if err != nil {
-		log.Fatal(err)
+func checkRunFlags(cmd *cobra.Command, args []string) {
+	if echo {
+		echoServer(cmd, args)
+	} else {
+		_ = cmd.Usage()
 	}
-	defer listener.Close()
 
+}
+
+func echoServer(cmd *cobra.Command, args []string) {
+	server, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+	if server == nil {
+		log.Panicf("couldn't start listening: %v", err)
+	} else {
+		log.Print("gtx is listening on " + strconv.Itoa(port))
+	}
+	connections := acceptConnections(server)
 	for {
-		log.Println("gtx server is listening to", port)
-		_, err := listener.Accept()
-		if err != nil {
-			log.Println(err)
+		go handleConn(<-connections)
+	}
+}
+
+func acceptConnections(listener net.Listener) chan net.Conn {
+	ch := make(chan net.Conn)
+	i := 0
+	go func() {
+		for {
+			client, err := listener.Accept()
+			if client == nil {
+				log.Panicf("couldn't accept: %v", err)
+			}
+			i++
+			fmt.Printf("%d: %v <-> %v\n", i, client.LocalAddr(), client.RemoteAddr())
+			ch <- client
 		}
-		break
+	}()
+	return ch
+}
+
+func handleConn(client net.Conn) {
+	b := bufio.NewReader(client)
+	for {
+		line, err := b.ReadBytes('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = client.Write(append([]byte("echo@gtx: "), line...))
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
